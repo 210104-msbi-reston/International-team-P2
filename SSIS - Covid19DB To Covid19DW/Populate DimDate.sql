d06fcb622fa83b9c23e029e80a3410a90e59c5af
mw5678 <weimin5678@outlook.com>
2021-02-12 16:47:07 -0500

    SSIS DB to DW changed age to age group

--- SSIS - Covid19DB To Covid19DW/Populate DimDate.sql
+++ SSIS - Covid19DB To Covid19DW/Populate DimDate.sql
@@ -2,12 +2,18 @@ USE Covid19DW
 GO
 DELETE FROM DimDate
 
+--Starting Date (The script will add this date, ensure that this date does not already exist)
+DECLARE @_StartDateString VARCHAR(10) = '2020-01-01'
+--Number of days the script will add
+DECLARE @_DaysToAdd INT = 2000
+DECLARE @_StartDate DATETIME = CAST(@_StartDateString AS DATETIME)
 DECLARE @_i INT = 0
 
-WHILE @_i < 1095
+
+WHILE @_i < @_DaysToAdd
 BEGIN
 	INSERT INTO DimDate (DateKey, FullDate)
-	VALUES (@_i, CAST(CAST('2020-01-01' AS DATETIME)+@_i AS DATE))
+	VALUES (@_i, CAST(@_StartDate + @_i AS DATE))
 	SET @_i += 1
 END
 
@@ -18,4 +24,3 @@ SET DateKey = YEAR(FullDate)*10000 + MONTH(FullDate)*100 + DAY(FullDate)
 , CalendarDay = DAY(FullDate)
 , NameOfMonth = DATENAME(MONTH, FULLDATE)
 , NameOfDay = DATENAME(WEEKDAY, FULLDATE)
-
