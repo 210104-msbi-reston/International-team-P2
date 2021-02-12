b366781f9cc6a7df2b98045385a6f3ec6b97eb10
mw5678 <weimin5678@outlook.com>
2021-02-11 23:16:46 -0500

    dimensions finished

---
+++ SSIS - Covid19DB To Covid19DW/Create Ages Table in DB.sql
@@ -0,0 +1,16 @@
+USE Covid19DB
+
+DROP TABLE Ages
+
+CREATE TABLE Ages (
+	age INT PRIMARY KEY
+)
+
+DECLARE @_i INT = 1
+DECLARE @_MaxAge INT = 120
+WHILE @_i <= @_MaxAge
+BEGIN
+	INSERT INTO Ages
+	VALUES (@_i)
+	SET @_i += 1
+END
